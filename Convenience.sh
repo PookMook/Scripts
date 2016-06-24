dd79ccfdf93a66da9bb828e0fb1ac88bb101c9c0
Arthur Juchereau <PookMook@users.noreply.github.com>
2016-06-24 11:16:46 -0400

    unity tweaktool

--- Convenience.sh
+++ Convenience.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install gimp vlc -y
+sudo apt-get install gimp vlc unity-tweak-tool -y
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
