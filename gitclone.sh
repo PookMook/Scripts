deafa1a443e7eb27d3dc70e3dddb4346e6c8e7d8
Arthur Juchereau <PookMook@users.noreply.github.com>
2017-06-25 18:23:41 -0400

    Create gitclone.sh

---
+++ gitclone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ~/Documents
+mkdir git
+cd git
+git clone git@github.com:PookMook/wedding.git
+git clone git@github.com:EcrituresNumeriques/anthologie-API.git
+git clone git@github.com:EcrituresNumeriques/chaineEditorialeSP.git
+git clone git@github.com:EcrituresNumeriques/Lightium.git
+git clone git@github.com:EcrituresNumeriques/anthologie-apiLite.git
+git clone git@github.com:PiNinja/PiNinja.git
+git clone git@github.com:PiNinja/sendgrid-mailer.git
