05faf91f49b742378f47fd42730e46d8eadbb111
Arthur <arthur.juchereau@gmail.com>
2018-09-08 12:18:14 -0400

    seems fixed

--- bibparse.sh
+++ bibparse.sh
@@ -9,11 +9,11 @@ fi
 
 for filename in ${1}/*.bib; do
 # remove previous keywords
-    sed -E 's/^\s*keywords = \{.*\}(,)?\s?[\r]?[\n]?$//g'  < $filename
+    sed -E 's/keywords = \{[a-zA-Z0-9\,\ ]*\},?//g' $filename
 
 # Generate new keywords
     echo "path : $filename"
     FILE=${filename##*/}
-    gawk -v filename="${FILE%%.bib}" 'match($0, /@(.+){(.+),/, m){ print "@"m[1]"{"m[2]",\nkeywords = {article/"filename",cleBib/"m[2]"},\n"}' < $filename
+    gawk -v filename="${FILE%%.bib}" 'match($0, /@(.+){(.+),/, m){ print "@"m[1]"{"m[2]",\nkeywords = {article/"filename",cleBib/"m[2]"},\n"}' $filename
 
 done
