2286e630655faa3f2cfba29287928ae7067d079d
Arthur <arthur.juchereau@gmail.com>
2018-09-08 11:51:54 -0400

    Work on file

--- bibparse.sh
+++ bibparse.sh
@@ -9,11 +9,11 @@ fi
 
 for filename in ${1}/*.bib; do
 # remove previous keywords
-    cat $filename | sed -E 's/^\s*keywords = \{.*\}(,)?\s?[\r]?[\n]?$//g'
+    sed -E 's/^\s*keywords = \{.*\}(,)?\s?[\r]?[\n]?$//g'  < $filename
 
 # Generate new keywords
     echo "path : $filename"
     FILE=${filename##*/}
-    cat $filename | gawk -v filename="${FILE%%.bib}" 'match($0, /@(.+){(.+),/, m){ print "@"m[1]"{"m[2]",\nkeywords = {article/"filename",cleBib/"m[2]"},\n"}'
+    gawk -v filename="${FILE%%.bib}" 'match($0, /@(.+){(.+),/, m){ print "@"m[1]"{"m[2]",\nkeywords = {article/"filename",cleBib/"m[2]"},\n"}' < $filename
 
 done
